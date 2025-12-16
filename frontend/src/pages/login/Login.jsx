e6ff374d953e3b428f56ec1dd7570d06952cd88a
Apeksha Dhungana <dhunganaapekshya3@gmail.com>
2025-12-16 09:36:46 +0545

    aajai baki xa hai clone garna css login ra register ko matra haleko ho

--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -55,7 +55,7 @@ const Login = () => {
 
   return (
     <div className="login-container">
-      <h2>Login</h2>
+      <h2 >Login</h2>
 
       <form onSubmit={handleSubmit}>
         <div className="form-group">
